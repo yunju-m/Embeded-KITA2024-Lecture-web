ad6db72a571018c2a257b084bb1a3770f984e7fa
yunju <dbswnfnal@naver.com>
2024-07-17 11:38:21 +0900

    240717 입력받은 정보를 useBean에 Property값 설정
    - form의 input name의 값과  useBean의 property의 명 통일
    - 만약 다를시, param을 별도로 선언
    - 동일한 name에 대해 '*'로 전체 property 선언 가능

---
+++ jspbasic/src/main/java/jspbasic/Person2.java
@@ -0,0 +1,57 @@
+package jspbasic;
+
+public class Person2 {
+	private String name;
+	private int age;
+	private String phone;
+	private String email;
+	
+	public Person2() {
+	}
+
+	public Person2(String name, int age, String phone, String email) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Person2 [name=" + name + ", age=" + age + ", phone=" + phone + ", email=" + email + "]";
+	}
+	
+}
