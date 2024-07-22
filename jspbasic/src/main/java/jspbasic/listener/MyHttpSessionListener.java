9a12fd74424283ee9b625e160c28815f2e03c0db
yunju <dbswnfnal@naver.com>
2024-07-22 11:40:46 +0900

    240722 Listener - HttpSessionListener
    - 로그인, 로그아웃 시 세션이 생성, 종료된다.

---
+++ jspbasic/src/main/java/jspbasic/listener/MyHttpSessionListener.java
@@ -0,0 +1,18 @@
+package jspbasic.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class MyHttpSessionListener implements HttpSessionListener {
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		System.out.println("세션 생성됨!");
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		System.out.println("세션 소멸됨!");
+	}
+
+}
