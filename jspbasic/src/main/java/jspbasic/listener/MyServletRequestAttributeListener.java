7c3c4b2141ddb1625b00e40144009e5e152b640b
yunju <dbswnfnal@naver.com>
2024-07-22 11:54:14 +0900

    240722 Listener - ServletRequestAttributeListener

---
+++ jspbasic/src/main/java/jspbasic/listener/MyServletRequestAttributeListener.java
@@ -0,0 +1,23 @@
+package jspbasic.listener;
+
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+
+public class MyServletRequestAttributeListener implements ServletRequestAttributeListener {
+
+	@Override
+	public void attributeAdded(ServletRequestAttributeEvent srae) {
+		System.out.println("request 속성 추가!");
+	}
+	
+	@Override
+	public void attributeRemoved(ServletRequestAttributeEvent srae) {
+		System.out.println("request 속성 삭제!");
+	}
+
+	@Override
+	public void attributeReplaced(ServletRequestAttributeEvent srae) {
+		System.out.println("request 속성 대체!");
+	}
+	
+}
