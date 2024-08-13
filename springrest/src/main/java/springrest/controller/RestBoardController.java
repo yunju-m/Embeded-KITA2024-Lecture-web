a5b88e0f7b6b69a99c60d63350e541e9e675480d
yunju <dbswnfnal@naver.com>
2024-08-13 10:45:02 +0900

    240813 [RestBoard] rest방식으로 GET, POST, PUT, DELETE

--- springrest/src/main/java/springrest/controller/RestBoardController.java
+++ springrest/src/main/java/springrest/controller/RestBoardController.java
@@ -1,7 +1,16 @@
 package springrest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import springrest.domain.RestBoard;
@@ -13,9 +22,36 @@ public class RestBoardController {
 	@Autowired
 	private RestBoardService restBoardService;
 	
-	@PostMapping(value="/board")
-	public int insertRestBoard(RestBoard restBoard) throws Exception {
+	// GET /board 요청하면 RestBoard에 담고 있는 List와 전송헤더데이터를 JSON으로 변환해서 리턴
+	@GetMapping(value="/board")
+	public ResponseEntity<List<RestBoard>> listRestBoard() throws Exception {
+		List<RestBoard> restBoardList = restBoardService.listRestBoard();
+		return ResponseEntity.ok(restBoardList); // statusCode 200 (정상응답)
+	}
+	
+	// GET /board/1 요청하면 RestBoard와 전송헤더데이터를 JSON으로 변환해서 리턴
+	// 요청 URI에 {}안에 변수명, @PathVariable과 매핑
+	@GetMapping(value="/board/{rbid}")
+	public ResponseEntity<RestBoard> getRestBoard(@PathVariable int rbid) throws Exception {
+		RestBoard restBoard = restBoardService.getRestBoard(rbid);
+		return ResponseEntity.ok(restBoard);
+	}
+	
+	// POST /board 요청하면
+	// producer-consumer 패턴
+	@PostMapping(value="/board", produces = MediaType.APPLICATION_JSON_VALUE)
+	public int insertRestBoard(@RequestBody RestBoard restBoard) throws Exception {
 		return restBoardService.insertRestBoard(restBoard);
+	}
+	
+	@PutMapping(value="/board", produces = MediaType.APPLICATION_JSON_VALUE)
+	public int updateRestBoared(@RequestBody RestBoard restBoard) throws Exception {
+		return restBoardService.updateRestBoard(restBoard);
+	}
+	
+	@DeleteMapping(value="/board/{rbid}")
+	public int deleteRestBoard(@PathVariable int rbid) throws Exception {
+		return restBoardService.deleteRestBoard(rbid);
 	}
 	
 }
